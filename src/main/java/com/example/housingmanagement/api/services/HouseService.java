f3a3451bbd1578428167b2966f15b37820223290
WojOgi <w.ogieglo@gmail.com>
2023-09-03 22:20:53 +0300

    prepared separate service classes

--- src/main/java/com/example/housingmanagement/api/services/HouseService.java
+++ src/main/java/com/example/housingmanagement/api/services/HouseService.java
@@ -1,4 +1,10 @@
 package com.example.housingmanagement.api.services;
 
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
 public class HouseService {
 }
