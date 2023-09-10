d5e67b0af86c2c2a86915d00c0cddd464bcb96b1
WojOgi <w.ogieglo@gmail.com>
2023-09-10 16:23:41 +0300

    KEL-78 Cleaned up the Response headers and also included some minor changes that should have gone to KEL-80 - removal of internal entity objects in controllers - code refactoring.

--- src/main/java/com/example/housingmanagement/api/controllers/HouseController.java
+++ src/main/java/com/example/housingmanagement/api/controllers/HouseController.java
@@ -1,11 +1,10 @@
 package com.example.housingmanagement.api.controllers;
 
-import com.example.housingmanagement.api.dbentities.HouseInternalEntity;
 import com.example.housingmanagement.api.mappers.HouseMapperInterface;
 import com.example.housingmanagement.api.requests.HouseRequest;
 import com.example.housingmanagement.api.responses.HouseResponse;
 import com.example.housingmanagement.api.services.HouseService;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,47 +22,39 @@ public class HouseController {
 
     @GetMapping(value = "/housing")
     public ResponseEntity<List<HouseResponse>> getAllHouses() {
-        HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("get", "get all Houses from the Database");
-
-        List<HouseInternalEntity> allHouseInternalEntities = houseService.fetchAll();
-        List<HouseResponse> houseResponses = houseMapper.toHouseResponse(allHouseInternalEntities);
-
-        return ResponseEntity.ok().headers(responseHeader).body(houseResponses);
+        List<HouseResponse> houseResponses = houseMapper.toHouseResponse(houseService.fetchAll());
+        return ResponseEntity.ok().body(houseResponses);
     }
 
     @PostMapping(value = "/housing")
-    public ResponseEntity<String> addNewHouse(@RequestBody HouseRequest houseToBeAdded) {
-        HttpHeaders responseHeaders = new HttpHeaders();
+    public ResponseEntity<Void> addNewHouse(@RequestBody HouseRequest houseToBeAdded) {
         //checks if this house exists
         if (houseService.existsByHouse(houseToBeAdded)) {
-            return ResponseEntity.badRequest().headers(responseHeaders).body(
-                    "A House with number " + houseToBeAdded.getHouseNumber() + " already exists");
+            System.out.println("A House with number " + houseToBeAdded.getHouseNumber() + " already exists");
+            return ResponseEntity.badRequest().build();
         }
         houseService.addHouseToDatabase(houseMapper.toHouseInternalEntity(houseToBeAdded));
-        responseHeaders.set("post", "adding a new House to the Database");
+        System.out.println("Added House with address: " + houseToBeAdded.getHouseNumber());
 
-        return ResponseEntity.ok().headers(responseHeaders).body("Added House with address: "
-                + houseToBeAdded.getHouseNumber());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping(value = "/housing")
-    public ResponseEntity<String> deleteSpecificHouse(@RequestBody HouseRequest houseToBeDeleted) {
-        HttpHeaders responseHeaders = new HttpHeaders();
+    public ResponseEntity<Void> deleteSpecificHouse(@RequestBody HouseRequest houseToBeDeleted) {
         //check if such house exists in Database
         if (!houseService.existsByHouse(houseToBeDeleted)) {
-            return ResponseEntity.badRequest().headers(responseHeaders).body(
-                    "There is no house with number " + houseToBeDeleted.getHouseNumber() + " in the Database.");
+            System.out.println("There is no house with number " + houseToBeDeleted.getHouseNumber() + " in the Database.");
+            return ResponseEntity.badRequest().build();
         }
         //check if the House is occupied by anybody
         if (houseService.houseCurrentCapacity(houseToBeDeleted) > 0) {
-            return ResponseEntity.badRequest().headers(responseHeaders).body(
-                    "This house is occupied. House needs to be vacated before removal from the Database.");
+            System.out.println("This house is occupied. House needs to be vacated before removal from the Database.");
+            return ResponseEntity.badRequest().build();
         }
         houseService.deleteHouseFromDatabase(houseToBeDeleted);
+        System.out.println("House with address: " + houseToBeDeleted.getHouseNumber() + " was deleted from Database.");
 
-        return ResponseEntity.ok().headers(responseHeaders).body("House with address: "
-                + houseToBeDeleted.getHouseNumber() + " was deleted from Database.");
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/com/example/housingmanagement/api/mappers/HouseMapper2.java
+++ src/main/java/com/example/housingmanagement/api/mappers/HouseMapper2.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 @Component
 public class HouseMapper2 implements HouseMapperInterface{
-
+    //this is a mock class to play around with how Spring decides which class to use for implementation
 
     @Override
     public List<HouseResponse> toHouseResponse(List<HouseInternalEntity> allHouseInternalEntities) {
