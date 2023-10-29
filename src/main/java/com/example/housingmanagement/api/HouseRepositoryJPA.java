91cfbf2b6664ca7f7332702604c4e67e4146cd41
WojOgi <w.ogieglo@gmail.com>
2023-10-29 14:06:24 +0300

    KEL-97 added Translactional annotations - not sure why it did not work via a normal pull request on git - maybe the change is too small (only added annotations)

--- src/main/java/com/example/housingmanagement/api/HouseRepositoryJPA.java
+++ src/main/java/com/example/housingmanagement/api/HouseRepositoryJPA.java
@@ -2,11 +2,14 @@ package com.example.housingmanagement.api;
 
 import com.example.housingmanagement.api.dbentities.HouseInternalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface HouseRepositoryJPA extends JpaRepository<HouseInternalEntity, Integer> {
 
     HouseInternalEntity findByHouseNumber(String houseNumber);
 
+    @Transactional
     void deleteByHouseNumber(String houseNumber);
+
     boolean existsByHouseNumber(String houseNumber);
 }
