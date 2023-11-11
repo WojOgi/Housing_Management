0bb344e512e91e06e986ebfa095aa9d8c5da2710
WojOgi <w.ogieglo@gmail.com>
2023-11-11 16:09:54 +0300

    KEL-116 added tests of POST method to make sure it can add a house if: a) database is empty b) contains some entries but non matches the requested house. Also, should not be able to add house if house with name equal to that from request exists in the database already.

--- src/test/java/com/example/housingmanagement/api/controllers/HouseControllerTest.java
+++ src/test/java/com/example/housingmanagement/api/controllers/HouseControllerTest.java
@@ -2,24 +2,33 @@ package com.example.housingmanagement.api.controllers;
 
 import com.example.housingmanagement.api.HouseRepositoryJPA;
 import com.example.housingmanagement.api.dbentities.HouseInternalEntity;
+import com.example.housingmanagement.api.requests.HouseRequest;
 import com.example.housingmanagement.api.responses.HouseResponse;
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -113,7 +122,8 @@ class HouseControllerTest {
 
         //when
         // Wykonaj żądanie HTTP GET na endpoint /houses
-        MvcResult result = mockMvc.perform(get("/houses/{id}", Integer.toString(id)).contentType(MediaType.APPLICATION_JSON))
+        MvcResult result = mockMvc
+                .perform(get("/houses/{id}", id).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
 
         // Pobierz zawartość odpowiedzi
@@ -124,6 +134,66 @@ class HouseControllerTest {
         assertEquals("House1", houseResponse.getHouseNumber());
         assertNotEquals("House2", houseResponse.getHouseNumber());
     }
+
+    @Test
+    @DisplayName("Should add house to empty database based on provided HouseRequest")
+    public void addNewHouseShouldAddHouseToEmptyDatabase() throws Exception {
+        //given
+        HouseRequest houseRequest = new HouseRequest("house1", 3);
+        String houseRequestJSONString = objectMapper.writeValueAsString(houseRequest);
+
+        //when
+        MvcResult result = mockMvc
+                .perform(post("/houses").contentType(MediaType.APPLICATION_JSON)
+                        .content(houseRequestJSONString))
+                .andExpect(status().isCreated()).andReturn();
+
+        //then
+        Assertions.assertEquals(201, result.getResponse().getStatus());
+    }
+
+    @Test
+    @DisplayName("Should add house to a populated database if no house matching HouseRequest exists")
+    public void addNewHouseShouldAddHouseToDatabase() throws Exception {
+        //given
+        //populate database
+        houseRepository.save(new HouseInternalEntity(now, "house0", 3, 0));
+        houseRepository.save(new HouseInternalEntity(now, "house1", 3, 0));
+
+        HouseRequest houseRequest = new HouseRequest("house2", 3);
+        String houseRequestJSONString = objectMapper.writeValueAsString(houseRequest);
+
+        //when
+        MvcResult result = mockMvc
+                .perform(post("/houses").contentType(MediaType.APPLICATION_JSON)
+                        .content(houseRequestJSONString))
+                .andExpect(status().isCreated()).andReturn();
+
+        //then
+        Assertions.assertEquals(201, result.getResponse().getStatus());
+    }
+
+    @Test
+    @DisplayName("Should NOT add house to database which contains house with houseNumber equal to that from HouseRequest")
+    public void addNewHouseShouldNotAddHouseToDatabase() throws Exception {
+        //given
+        //populate database
+        houseRepository.save(new HouseInternalEntity(now, "house0", 3, 0));
+        houseRepository.save(new HouseInternalEntity(now, "house1", 3, 0));
+
+        HouseRequest houseRequest = new HouseRequest("house1", 3);
+        String houseRequestJSONString = objectMapper.writeValueAsString(houseRequest);
+
+        //when
+        MvcResult result = mockMvc
+                .perform(post("/houses").contentType(MediaType.APPLICATION_JSON)
+                        .content(houseRequestJSONString))
+                .andExpect(status().isUnprocessableEntity()).andReturn();
+
+        //then
+        Assertions.assertEquals(422, result.getResponse().getStatus());
+    }
+
 
 
 
