72f8ed6ee90520ff599147759691f7d7b497392d
WojOgi <w.ogieglo@gmail.com>
2023-11-26 10:04:52 +0300

    KEL-124 introduced the WebConfig to fix the problem with context() while running tests

--- src/test/java/com/example/housingmanagement/HousingmanagementApplicationTests.java
+++ src/test/java/com/example/housingmanagement/HousingmanagementApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.housingmanagement;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class HousingmanagementApplicationTests {
-
 
     @Test
     void contextLoads() {
